51d5b1da1c72bf24a0da23cd269fa00d6c16dd9a
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2025-08-18 13:39:39 +0800

    primary changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,86 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Navbar from "react-bootstrap/Navbar";
+import {Row, Col, Container} from "react-bootstrap";
+import  Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <section>
+        {/* NAVBAR */}
+        <Navbar bg="light" expand="lg" className="mb-3">
+          <Container>
+            {/* <Navbar.Brand href="#home">Portfolio</Navbar.Brand> */}
+            <Nav className="ms-auto">
+              <Button variant="outline-sucess">Download CV</Button>
+            </Nav>
+          </Container>
+        </Navbar>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        {/* FIRST PART OF CV */}
+        <Container fluid>
+          <Row className="w-100 mb-2">
+            {/* PROFILE */}
+            <Col md={4}>
+              <Card>
+                <Image
+                  src="/assets/CS16.jpg"
+                  width={300}
+                  height={400}
+                  alt="Profile"
+                />
+                <Card.Body>
+                  <Card.Title>Pajarilla, Gliezel Ann </Card.Title>
+                  <Card.Text>BS Computer Science</Card.Text>
+                  <Card.Text>Iloilo, Philippines</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            {/* Intro Text */}
+            <Col md={4}>
+              <h1 className="fw-bold">
+                Research-driven projects are the building blocks of our society
+              </h1>
+              <p className="fst-normal">
+                Extracting meaning and insights from data, creating projects
+                that are building blocks to something that creates impacts are
+                my inspiration. I am enthusiastic in working with projects that
+                promotes welfare and development.
+              </p>
+
+              <div className="d-flex gap-2 mb-3">
+                <Button variant="outline-primary">Download CV</Button>
+                <Button variant="outline-primary">Contact Me</Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <h1 className="fw-fold">Projects I'd Worked On</h1>
+        <div className="container">
+          <h5 className="fs-6">Featured Project</h5>
+          <h2 className="fw-fold">The Philippine Sponge Guide</h2>
+          <div className="container-text">
+            <h5 className="fs-6">
+              The Philippine Sponge Guide is a searchable database of
+              information on marine sponges in the Philippines as collected by
+              the Marine Science Institute of the University of the Philippines
+              Diliman (UP MSI).</h5>
+          </div>
+          <div className="container-bottom">
+            
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
     </div>
   );
 }
